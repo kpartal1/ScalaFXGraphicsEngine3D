399a93b9d0c5aa92047397501c8ec3b1d3bac109
Konstantine Partalas <kpartal1@trinity.edu>
2021-04-05 12:20:31 -0500

    Beginning work on 3D engine

--- src/main/scala/graphicsengine3d/Main.scala
+++ src/main/scala/graphicsengine3d/Main.scala
@@ -5,10 +5,10 @@ import scalafx.scene.canvas.Canvas
 import scalafx.scene.Scene
 
 object Main extends JFXApp {
-	val canvas = new Canvas(1920, 1080)
-	stage = new JFXApp.PrimaryStage {
-		title = "GraphicsEngine3D"
-		scene = new Scene(1920, 1080) {
-		}
-	}
+  val canvas = new Canvas(1920, 1080)
+    stage = new JFXApp.PrimaryStage {
+      title = "GraphicsEngine3D"
+      scene = new Scene(1920, 1080) {
+      }
+    }
 }

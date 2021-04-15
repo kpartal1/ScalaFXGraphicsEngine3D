fa8863428ab974d5c12064ddb7c9594a0d4387dc
Konstantine <kpartal1@trinity.edu>
2021-04-15 16:37:24 -0500

    FInally got it to display eat me ass bitch

--- src/main/scala/graphicsengine3d/Main.scala
+++ src/main/scala/graphicsengine3d/Main.scala
@@ -17,7 +17,7 @@ object Main extends JFXApp {
     o.z = i.x * m.m(0)(2) + i.y * m.m(1)(2) + i.z * m.m(2)(2) + m.m(3)(2)
     val w: Double = i.x * m.m(0)(3) + i.y * m.m(1)(3) + i.z * m.m(2)(3) + m.m(3)(3)
 
-    if(w != 0) {
+    if(w != 0.0) {
       o.x /= w
       o.y /= w
       o.z /= w
@@ -41,36 +41,8 @@ object Main extends JFXApp {
   val canvas = new Canvas(1920, 1080)
   val gc: GraphicsContext = canvas.getGraphicsContext2D()
   gc.fillRect(0.0, 0.0, canvas.getWidth(), canvas.getHeight())
-  val meshCube: Mesh = new Mesh
+  val meshCube: Mesh = meshObjects.meshCube
   val matProj: Mat4x4 = new Mat4x4
-  val vec1 = new Vec3d
-  meshCube.tris = List(
-    	
-    // SOUTH
-		( vec1.x = 0.0, 0.0, 0.0,    0.0, 1.0, 0.0,    1.0, 1.0, 0.0 ),
-		( 0.0, 0.0, 0.0,    1.0, 1.0, 0.0,    1.0, 0.0, 0.0 ),
-
-		// EAST                                                      
-		( 1.0, 0.0, 0.0,    1.0, 1.0, 0.0,    1.0, 1.0, 1.0 ),
-		( 1.0, 0.0, 0.0,    1.0, 1.0, 1.0,    1.0, 0.0, 1.0 ),
-
-		// NORTH                                                     
-		( 1.0, 0.0, 1.0,    1.0, 1.0, 1.0,    0.0, 1.0, 1.0 ),
-		( 1.0, 0.0, 1.0,    0.0, 1.0, 1.0,    0.0, 0.0, 1.0 ),
-
-		// WEST                                                      
-		( 0.0, 0.0, 1.0,    0.0, 1.0, 1.0,    0.0, 1.0, 0.0 ),
-		( 0.0, 0.0, 1.0,    0.0, 1.0, 0.0,    0.0, 0.0, 0.0 ),
-
-		// TOP                                                       
-		( 0.0, 1.0, 0.0,    0.0, 1.0, 1.0,    1.0, 1.0, 1.0 ),
-		( 0.0, 1.0, 0.0,    1.0, 1.0, 1.0,    1.0, 1.0, 0.0 ),
-
-		// BOTTOM                                                    
-		( 1.0, 0.0, 1.0,    0.0, 0.0, 1.0,    0.0, 0.0, 0.0 ),
-		( 1.0, 0.0, 1.0,    0.0, 0.0, 0.0,    1.0, 0.0, 0.0 ),
-
-		)
   // Projection Matrix
   val fNear: Double = 0.1
   val fFar: Double = 1000.0
@@ -88,6 +60,7 @@ object Main extends JFXApp {
       scene = new Scene(1920, 1080) {
         content += canvas
         var lastTime = -1L
+        var count = 0
 		    val timer = AnimationTimer { time =>
 				if(lastTime >= 0) {
 					val delay = (time - lastTime) / 1e9
@@ -95,6 +68,7 @@ object Main extends JFXApp {
           val matRotZ: Mat4x4 = new Mat4x4
           val matRotX: Mat4x4= new Mat4x4
           fTheta += 1 * delay
+          count += 1
 
           // Rotation Z
           matRotZ.m(0)(0) = math.cos(fTheta);
@@ -146,7 +120,8 @@ object Main extends JFXApp {
             triProjected.p(1).x *= 0.5 * canvas.getWidth(); triProjected.p(1).y *= 0.5 * canvas.getHeight()
             triProjected.p(2).x *= 0.5 * canvas.getWidth(); triProjected.p(2).y *= 0.5 * canvas.getHeight()
 
-            drawTriangle(triProjected.p(0).x, triProjected.p(0).y, triProjected.p(1).x, triProjected.p(1).y, triProjected.p(2).x, triProjected.p(2).x)
+            drawTriangle(triProjected.p(0).x, triProjected.p(0).y, triProjected.p(1).x, triProjected.p(1).y, triProjected.p(2).x, triProjected.p(2).y)
+            if(count == 1) gc.fillRect(0.0, 0.0, canvas.getWidth(), canvas.getHeight()); count = 0
           }
 				}
 				lastTime = time
--- src/main/scala/graphicsengine3d/Triangle.scala
+++ src/main/scala/graphicsengine3d/Triangle.scala
@@ -1,5 +1,5 @@
 package graphicsengine3d
 
 class Triangle {
-  var p: Array[Vec3d] = Array.fill(3){Nil.asInstanceOf[Vec3d]}
+  var p: Array[Vec3d] = Array.fill(3){new Vec3d}
 }
